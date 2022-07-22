1783f1157587ac2e6aebd9afb9a7eba42c47ad44
Joe <106337992+BladeJoe@users.noreply.github.com>
2022-07-22 19:06:51 +0500

    new

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+var image = document.querySelector(".image");
+var wrapper = document.querySelector(".wrapper");
+var movie_title = document.querySelector(".movie-title");
+
+
+for (let i = 0; i < movies.length; i++) {  
+
+}

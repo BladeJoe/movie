eaa267fa2bd97f2c865275e83f38ea6806e445ae
Joe <106337992+BladeJoe@users.noreply.github.com>
2022-07-30 21:12:09 +0500

    seearch by

--- js/main.js
+++ js/main.js
@@ -1,68 +1,112 @@
- let elList = document.querySelector(".movie__wrapper");
- let elForm = document.querySelector(".form ");
- let elSearch = document.querySelector("#textBySearch");
- let elRating = document.querySelector("#searchByRating");
- elList.innerHTML = null;
-
- let moviesArray = movies.slice(0, 50);
- elForm.addEventListener("submit", function (evt) {
-     evt.preventDefault();
-
-     renderMovies(newArray, elMovieWrapper)
- })
-
- function normolize(array) {
-     let newArray = [];
+let elMovieWrapper = document.querySelector(".movie__wrapper");
+let elTemplate = document.querySelector("#movie_card").content;
+let elTotal = document.querySelector(".total");
+let elList = document.querySelector(".movie__wrapper");
+let elForm = document.querySelector(".form ");
+let elSearch = document.querySelector("#SearchBySearch");
+let elRating = document.querySelector("#searchByRating");
+let elYear = document.querySelector("#searchByYear");
+let elSelectCategories = document.querySelector(".select__categories");
 
 
-     array.forEach(item => {
-         let newObject = {}
-
-         newObject.title = item.Title.toString();
-         newObject.videoUrl = `https://www.youtube.com/watch?v=${item.ytid}`;
-         newObject.categories = item.Categories.split("|");
-         newObject.movieYear = item.movie_year;
-         newObject.rating = item.imdb_rating;
-         newObject.image = `https://img.youtube.com/vi/${item.ytid}/default.jpg`;
-
-         newArray.push(newObject)
-     });
+let moviesArray = movies.slice(0, 100);
 
 
 
-     return newArray
- }
+let normolizedArray = moviesArray.map(item => {
+    return {
+        title: item.Title.toString(),
+        videoUrl: `https://www.youtube.com/watch?v=${item.ytid}`,
+        categories: item.Categories.split("|"),
+        movieYear: item.movie_year,
+        rating: item.imdb_rating,
+        image: `https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`
+    }
+});
 
 
- let elMovieWrapper = document.querySelector(".movie__wrapper");
- let elTemplate = document.querySelector("#movie_card").content;
+elForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    renderMovies(normolizedArray, elMovieWrapper)
+})
 
 
- let newArray = normolize(moviesArray);
+
+function getCategories(array) {
+
+    let categoriesArray = [];
+    for (const item of array) {
+        for (const itemCategory of item.categories) {
+            if (!categoriesArray.includes(itemCategory)) {
+                categoriesArray.push(itemCategory)
+            }
+        }
+    }
+
+    return categoriesArray
+}
+let movieCategories = getCategories(normolizedArray)
+
+function renderMovies(array, wrapper) {
+    wrapper.innerHTML = null;
 
 
- function renderMovies(array, wrapper) {
-     wrapper.innerHTML = null
 
-     let tempFragment = document.createDocumentFragment()
+    let tempFragment = document.createDocumentFragment();
+    let rendered_movies = 0;
+    for (const item of array) {
+        let templateItem = elTemplate.cloneNode(true)
+        if (elSelectCategories.value == "all") {
 
-     for (const item of array) {
-         if (elRating.value <= item.rating) {
-             let templateItem = elTemplate.cloneNode(true)
-             templateItem.querySelector(".movie__img").src = item.image;
-             templateItem.querySelector(".movie__title").textContent = item.title;
-             templateItem.querySelector(".movie__year").innerHTML = `<img class="" src="./images/calendar.png" alt="" width="20" height="20">${item.movieYear}`;
-             templateItem.querySelector(".movie__rating").innerHTML = `<img class="" src="./images/star.png" alt="" width="20" height="20">${item.rating}`;
-             templateItem.querySelector(".movie__url").href = item.videoUrl;
+            if (elRating.value <= item.rating && elYear.value <= item.movieYear) {
+                templateItem.querySelector(".movie__img").src = item.image;
+                templateItem.querySelector(".movie__title").textContent = item.title;
+                templateItem.querySelector(".movie__year").textContent = item.movieYear;
+                templateItem.querySelector(".movie__rating").textContent = item.rating;
+                templateItem.querySelector(".movie__categories").textContent = item.categories;
+                templateItem.querySelector(".movie__url").href = item.videoUrl;
+                tempFragment.appendChild(templateItem)
+                rendered_movies++;
+            }
+        } else if (elSelectCategories.value != "all") {
+            if (elRating.value <= item.rating && elYear.value <= item.movieYear && elSelectCategories.value == item.categories) {
+                templateItem.querySelector(".movie__img").src = item.image;
+                templateItem.querySelector(".movie__title").textContent = item.title;
+                templateItem.querySelector(".movie__year").textContent = item.movieYear;
+                templateItem.querySelector(".movie__rating").textContent = item.rating;
+                templateItem.querySelector(".movie__categories").textContent = item.categories;
+                templateItem.querySelector(".movie__url").href = item.videoUrl;
+                tempFragment.appendChild(templateItem)
+                rendered_movies++;
+            }
+        }
+    }
 
-             tempFragment.appendChild(templateItem)
-         }
+    elTotal.textContent = rendered_movies;
+    if (rendered_movies != 0) {
+        wrapper.appendChild(tempFragment)
+    } else {
+        wrapper.innerHTML = "<h1>0 Movies found</h1>"
 
-     }
-     wrapper.appendChild(tempFragment)
+    }
 
- }
 
- 
+}
 
- renderMovies(newArray, elMovieWrapper)
+function renderCategories(array, wrapper) {
+    let tempFragment = document.createDocumentFragment();
+
+    for (const item of array) {
+        let newOption = document.createElement("option");
+        newOption.textContent = item;
+        newOption.value = item;
+
+        tempFragment.appendChild(newOption);
+    }
+
+    wrapper.appendChild(tempFragment)
+}
+
+
+renderMovies(normolizedArray, elMovieWrapper)
+renderCategories(movieCategories, elSelectCategories)
